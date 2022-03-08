4d173fc62d17ba0779d7f0bd4ba93f05e77f4c36
Adriano Faustino <faustino.adr@gmail.com>
2022-03-08 12:16:58 -0300

    starting implementation of hash table

--- src/minishell.c
+++ src/minishell.c
@@ -3,17 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: adrianofaus <adrianofaus@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 21:30:44 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/03/07 22:01:40 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/03/08 12:06:00 by adrianofaus      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
 
-int	main(int argc, char *argv[])
+void	insert_in_hastable(char *current_variable)
+{
+	//Função que vai inserir dentro da hash
+	//Criar função hash
+}
+
+void	get_env_variables(char **envp)
+{
+	int		i;
+
+	i = -1;
+	while (envp[++i])
+	{
+		insert_in_hashtable(envp[i]);
+	}
+}
+
+int	main(int argc, char *argv[], char **envp)
 {
 	char	*line_read;
 
@@ -22,6 +39,7 @@ int	main(int argc, char *argv[])
 	if (argc != 1)
 		return (1);
 	write(1, "Hello World !\n", 14);
+	get_env_variables(envp);
 	while (1)
 	{
 		if (line_read)
