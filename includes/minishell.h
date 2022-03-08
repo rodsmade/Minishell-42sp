984f9ab3e669ad7a8ce4a5946249f2a1255db1bb
rodsmade <rodrigogregoldo@gmail.com>
2022-03-07 22:02:06 -0300

    norminetting!

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/07 22:01:44 by roaraujo          #+#    #+#             */
+/*   Updated: 2022/03/07 22:01:51 by roaraujo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -7,6 +19,5 @@
 # include <unistd.h>	// write(), close()
 # include <stdlib.h>	// free()
 # include "libft.h"
-
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 21:30:44 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/03/07 21:52:24 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/03/07 22:01:40 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int	main(int argc, char *argv[])
 	line_read = NULL;
 	if (argc != 1)
 		return (1);
-
 	write(1, "Hello World !\n", 14);
 	while (1)
 	{
@@ -30,13 +29,12 @@ int	main(int argc, char *argv[])
 		line_read = readline("Type yr command (type \'quit\' to exit): ");
 		add_history(line_read);
 		if (ft_strncmp(line_read, "quit", 5) == 0)
-			break;
+			break ;
 	}
 	ft_free_ptr((void *)&line_read);
 	rl_clear_history();
 	close(0);
 	close(1);
 	close(2);
-
 	return (0);
 }
