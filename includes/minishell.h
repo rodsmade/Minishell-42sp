c5446011024efde7ac4030185baade6ad3f88c7d
rodsmade <rodrigogregoldo@gmail.com>
2022-03-07 21:53:03 -0300

    included libft

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,11 +1,12 @@
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 // --------------------------------------------	INCLUDES	--------------------
 # include <readline/readline.h>	// readline(), etc.
 # include <readline/history.h>	// add_history()
 # include <unistd.h>	// write(), close()
 # include <stdlib.h>	// free()
+# include "libft.h"
 
 
 #endif
