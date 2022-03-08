429ed420726d46aa4905be7d1fbb6b7d55b4941a
rodsmade <rodrigogregoldo@gmail.com>
2022-03-08 12:35:59 -0300

    separated readline from main into repl() function; declared hashtable and added function to initialise it; lacking tests

--- src/minishell.c
+++ src/minishell.c
@@ -3,54 +3,85 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adrianofaus <adrianofaus@student.42.fr>    +#+  +:+       +#+        */
+/*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 21:30:44 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/03/08 12:06:00 by adrianofaus      ###   ########.fr       */
+/*   Updated: 2022/03/08 12:35:10 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
 
-void	insert_in_hastable(char *current_variable)
+# define	TABLE_SIZE	50
+
+typedef struct s_env_var
 {
-	//Função que vai inserir dentro da hash
-	//Criar função hash
-}
+	char				*name;
+	char				*content;
+	struct s_env_var	*next;
+}				t_env_var;
 
-void	get_env_variables(char **envp)
-{
-	int		i;
-
-	i = -1;
-	while (envp[++i])
-	{
-		insert_in_hashtable(envp[i]);
-	}
-}
-
-int	main(int argc, char *argv[], char **envp)
+void	repl(void)
 {
 	char	*line_read;
 
-	(void)argv;
 	line_read = NULL;
-	if (argc != 1)
-		return (1);
-	write(1, "Hello World !\n", 14);
-	get_env_variables(envp);
 	while (1)
 	{
-		if (line_read)
-			ft_free_ptr((void *)&line_read);
 		line_read = readline("Type yr command (type \'quit\' to exit): ");
 		add_history(line_read);
 		if (ft_strncmp(line_read, "quit", 5) == 0)
 			break ;
+		if (line_read)
+			ft_free_ptr((void *)&line_read);
 	}
 	ft_free_ptr((void *)&line_read);
 	rl_clear_history();
+	return ;
+}
+
+void	insert_in_hashtable(char *current_variable)
+{
+	//Função que vai inserir dentro da hash
+	//Criar função hash
+	(void) current_variable;
+}
+
+void	init_hashtable(t_env_var *(*hashtable)[TABLE_SIZE])
+{
+	int	i;
+
+	i = -1;
+	while (++i < TABLE_SIZE)
+	{
+		*hashtable[i] = NULL;
+	}
+	return ;
+}
+
+void	get_env_variables(char **envp)
+{
+	int			i;
+	t_env_var	*hashtable[TABLE_SIZE];
+
+	i = -1;
+	init_hashtable(&hashtable);
+	while (envp[++i])
+	{
+		insert_in_hashtable(envp[i]);
+	}
+	return ;
+}
+
+int	main(int argc, char *argv[], char **envp)
+{
+	(void)argv;
+	if (argc != 1)
+		return (1);
+	write(1, "Hello World !\n", 14);
+	get_env_variables(envp);
+	repl();
 	close(0);
 	close(1);
 	close(2);
