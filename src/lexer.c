1f01641252514a18cae6c743aa052b1df82d8889
Adriano Faustino <faustino.adr@gmail.com>
2022-03-11 12:32:31 -0300

    Token generator done, still need to apply norms

--- src/lexer.c
+++ src/lexer.c
@@ -6,35 +6,146 @@
 /*   By: adrianofaus <adrianofaus@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 15:00:45 by adrianofaus       #+#    #+#             */
-/*   Updated: 2022/03/10 18:31:58 by adrianofaus      ###   ########.fr       */
+/*   Updated: 2022/03/11 12:29:31 by adrianofaus      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	count_fields(char *line_read)
+extern t_tudao	g_tudao;
+
+int	count_redirect(char *line_read)
+{
+	int	i;
+
+	i = 0;
+	if ((line_read[i] == '>' && line_read[i + 1] && line_read[i + 1] == '>') || \
+		(line_read[i] == '<' && line_read[i + 1] && line_read[i + 1] == '<') || \
+		(line_read[i] == '&' && line_read[i + 1] && line_read[i + 1] == '&') || \
+		(line_read[i] == '|' && line_read[i + 1] && line_read[i + 1] == '|'))
+		return (2);
+	else if (line_read[i] == '>' || line_read[i] == '<' || \
+			line_read[i] == '&' || line_read[i] == '|')
+		return (1);
+	else
+		return (0);
+}
+
+void	redirect_gen(char *line_read, char *content)
+{
+	int	i;
+	
+	i = 0;
+	if ((line_read[i] == '>' && line_read[i + 1] && line_read[i + 1] == '>') || \
+		(line_read[i] == '<' && line_read[i + 1] && line_read[i + 1] == '<') || \
+		(line_read[i] == '&' && line_read[i + 1] && line_read[i + 1] == '&') || \
+		(line_read[i] == '|' && line_read[i + 1] && line_read[i + 1] == '|'))
+	{
+		content[i] = line_read[i];
+		content[i + 1] = line_read[i + 1];
+		content[i + 2] = '\0';
+	}
+	else if (line_read[i] == '>' || line_read[i] == '<' || \
+			line_read[i] == '&' || line_read[i] == '|')
+	{
+		content[i] = line_read[i];
+		content[i + 1] = '\0';
+	}
+}
+
+int	token_len(char *line_read)
 {
 	int		i;
-	int		fields;
-	int		into_word;
-
-	i = -1;
-	fields = 0;
-	into_word = 0;
-	while (line_read[++i])
+	int		redirect;
+	char	quote_type;
+	
+	i = 0;
+	redirect = count_redirect(&line_read[i]);
+	if (!redirect)
 	{
-		space_iter(&line_read[i], &into_word, &i);
-		fields += check_char(&line_read[i], &i, &into_word);
-		if (line_read[i] == 39 || line_read[i] == 34)
-			fields += quote_iter(&line_read[i], &i, &into_word);
+		while (line_read[i] && line_read[i] != ' ' && !redirect)
+		{
+			if (line_read[i] == 39 || line_read[i] == 34)
+			{
+				quote_type = line_read[i];
+				i++;
+				while (line_read[i] && line_read[i] != quote_type)
+					i++;
+			}
+			if (line_read[i])
+			{
+				i++;
+				redirect = count_redirect(&line_read[i]);
+			}
+		}
+		return (i);
 	}
-	return (fields);
+	return (redirect);
 }
 
-void	lexer_line(char	*line_read)
+void	tokenizer(char *line_read, char *content)
 {
-	int		fields;
+	int		i;
+	int		redirect;
+	char	quote_type;
 
-	fields = count_fields(line_read);
-	printf(">: fields %d\n", fields);
+	i = 0;
+	redirect = count_redirect(&line_read[i]);
+	if (!redirect)
+	{
+		while (line_read[i] && line_read[i] != ' ' && !redirect)
+		{
+			if (line_read[i] == 39 || line_read[i] == 34)
+			{
+				quote_type = line_read[i];
+				content[i] = line_read[i];
+				i++;
+				while (line_read[i] && line_read[i] != quote_type)
+				{
+					content[i] = line_read[i];
+					i++;
+				}
+			}
+			if (line_read[i])
+			{
+				content[i] = line_read[i];
+				i++;
+				redirect = count_redirect(&line_read[i]);
+			}
+		}
+		content[i] = '\0';
+	}
+	redirect_gen(&line_read[i], content);
 }
+
+void	lexer_line(char *line_read)
+{
+	t_list	*head;
+	char	*content;
+	int		len;
+	int		i;
+
+	head = NULL;
+	i = 0;
+	len = 0;
+	while (line_read[i])
+	{
+		while (line_read[i] && line_read[i] == ' ')
+			i++;
+		len = token_len(&line_read[i]);
+		if (len)
+		{
+			content = (char *)malloc(sizeof(char) * (len + 1));
+			tokenizer(&line_read[i], content);
+			ft_lstadd_back(&head, ft_lstnew(content));
+			content = NULL;
+		}
+		i += len;
+	}
+	for (t_list	*pivot = head; pivot != NULL; pivot = pivot->next)
+	{
+		printf("{%s}", (char *)pivot->content);
+	}
+	printf("\n");
+}
+
