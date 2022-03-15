ff35ccb3881f6b80a10ec347c2ac80b17b57fdf3
Adriano Faustino <faustino.adr@gmail.com>
2022-03-15 13:30:53 -0300

    Lexer norminette done

--- misc/teste.c
+++ misc/teste.c
@@ -1,17 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   teste.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrianofaus <adrianofaus@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 13:21:37 by adrianofaus       #+#    #+#             */
+/*   Updated: 2022/03/15 13:27:41 by adrianofaus      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[], char *envp[])
+/* QUAL É A CONCLUSÃO?
+	// Dentro da função execve o primeiro argumento 
+	precisa necessariamente ser o comando
+	// "desexpandido", ou seja, se vier no input g"r"ep, 
+	o primeiro argumento de execve tem que
+	// ser "/usr/bin/grep", caso contrário dá ruim. 
+	e tudo bem dentro do array estar g"r"ep! 
+*/
+/*
+int	main(int argc, char *argv[], char *envp[])
 {
-	char *arr[] = {
-		"/usr/bin/ls",			// QUAL É A CONCLUSÃO?
-		"-la",						// Dentro da função execve o primeiro argumento precisa necessariamente ser o comando
-		NULL						// "desexpandido", ou seja, se vier no input g"r"ep, o primeiro argumento de execve tem que
-	};								// ser "/usr/bin/grep", caso contrário dá ruim. e tudo bem dentro do array estar g"r"ep!
-
+	char	*arr[] = { "/usr/bin/ls", "-la", NULL};
 	printf("%s\n", "\0");
 	printf("%p\n", arr[0]);
 	if (-1 == execve(arr[0], arr, envp))
 		printf("deu ruim\n");
 	return 0;
 }
+*/
--- misc/utils_test.c
+++ misc/utils_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_test.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: adrianofaus <adrianofaus@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 22:15:02 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/03/08 22:15:26 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/03/15 13:28:41 by adrianofaus      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,9 @@ void	print_hashtable(t_list *(*hashtable)[TABLE_SIZE])
 		printf("%d\t: ", i);
 		while (pivot != NULL)
 		{
-			printf("{nome: %s, value: %s}", ((t_env_var *)(pivot->content))->name, ((t_env_var *)(pivot->content))->value);
+			printf("{nome: %s, value: %s}", \
+			((t_env_var *)(pivot->content))->name, \
+			((t_env_var *)(pivot->content))->value);
 			if (pivot->next != NULL)
 				printf(",\n\t  ");
 			pivot = pivot->next;
