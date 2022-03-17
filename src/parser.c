7d2e5458615e3c38fdc3d0795b37998ce3405794
rodsmade <rodrigogregoldo@gmail.com>
2022-03-17 19:47:28 -0300

    implemented capture of redirects

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 16:58:27 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/03/17 18:24:20 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/03/17 19:44:29 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,22 +40,123 @@ void	check_tokens_consistency(void)
 			print_syntax_error_exit((char *) pivot->next->content);
 		pivot = pivot->next;
 	}
-	if (!g_tudao.syntax_error
-		&& is_redirect((char *) pivot->content) && !pivot->next)
+	if (!g_tudao.syntax_error && pivot && !pivot->next
+		&& is_redirect((char *) pivot->content))
 		print_syntax_error_exit("newline");
 	return ;
 }
 
-// TODO: implementar lógica de adicionar comando na lista de pipeline
-// while (token && token != '|')
-// {
-// 	if token = "<" or ">"
-// 		skip two tokens;
-// 	cmd_array = add_token_to_list(token);	// criar função de concatenação de
-// tokens! q faz realloc. 
-// }
+void	init_command(t_command *command)
+{
+	command->cmds_with_flags = NULL;
+	command->inputs = NULL;
+	command->outputs = NULL;
+	command->heredocs = NULL;
+	command->o_concats = NULL;
+	command->err = NULL;
+	return ;
+}
+
+void	capture_redirects(t_command	*cmd)
+{
+	t_list	*pivot;
+
+	pivot = g_tudao.token_list;
+	while (pivot && !is_pipe_and_or(pivot->content))
+	{
+		if (is_input(pivot))
+			ft_lstadd_back(&cmd->inputs, ft_lstnew(pivot->next->content));
+		if (is_output(pivot))
+			ft_lstadd_back(&cmd->outputs, ft_lstnew(pivot->next->content));
+		if (is_heredoc(pivot))
+			ft_lstadd_back(&cmd->heredocs, ft_lstnew(pivot->next->content));
+		if (is_o_concat(pivot))
+			ft_lstadd_back(&cmd->o_concats, ft_lstnew(pivot->next->content));
+		pivot = pivot->next;
+	}
+	return ;
+}
+
+void	print_redirects(void)
+{
+	t_list	*pivot_cmd;
+	t_list	*pivot_redirect;
+	int		i;
+
+	pivot_cmd = g_tudao.command_table.main_pipeline;
+	i = -1;
+	while (pivot_cmd)
+	{
+		printf("cmd[%i]'s redirects:\n", ++i);
+		pivot_redirect = ((t_command *) pivot_cmd->content)->inputs;
+		printf("\tinputs: ");
+		while (pivot_redirect)
+		{
+			printf("(%s), ", (char *) pivot_redirect->content);
+			pivot_redirect = pivot_redirect->next;
+		}
+		printf("\n");
+		pivot_redirect = ((t_command *) pivot_cmd->content)->outputs;
+		printf("\toutputs: ");
+		while (pivot_redirect)
+		{
+			printf("(%s), ", (char *) pivot_redirect->content);
+			pivot_redirect = pivot_redirect->next;
+		}
+		printf("\n");
+		pivot_redirect = ((t_command *) pivot_cmd->content)->heredocs;
+		printf("\theredocs: ");
+		while (pivot_redirect)
+		{
+			printf("(%s), ", (char *) pivot_redirect->content);
+			pivot_redirect = pivot_redirect->next;
+		}
+		printf("\n");
+		pivot_redirect = ((t_command *) pivot_cmd->content)->o_concats;
+		printf("\to_concats: ");
+		while (pivot_redirect)
+		{
+			printf("(%s), ", (char *) pivot_redirect->content);
+			pivot_redirect = pivot_redirect->next;
+		}
+		printf("\n");
+		pivot_cmd = pivot_cmd->next;
+	}
+	return ;
+}
+
+void	print_single_command_full()
+{
+
+}
+
+void	set_up_main_pipeline(void)
+{
+	t_command	*command;
+	// t_list		*pivot;
+
+	// pivot = g_tudao.token_list;
+	// while (pivot && !is_and_or(pivot->content))
+	// {
+	// 	{
+			command = malloc(sizeof(t_command));
+			if (!command)
+				printf("[ERROR] deu ruim o malloc ajustar isso aqui depois\n");
+			init_command(command);
+			capture_redirects(command);
+			ft_lstadd_back(&g_tudao.command_table.main_pipeline,
+				ft_lstnew((void *) command));
+			// capture_commands(command);
+	// 	}
+	// }
+}
+
 void	set_up_command_table(void)
 {
+	set_up_main_pipeline();
+	print_redirects();
+	// print_commands();
+	// free_commands();
 	return ;
 }
 
