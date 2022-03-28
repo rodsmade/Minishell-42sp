90c4baeb3ea12c30819cfe74908d5fc6dee20c2e
Adriano Faustino <faustino.adr@gmail.com>
2022-03-28 17:15:22 -0300

    testing expansor

--- src/expansor.c
+++ src/expansor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expansor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*   By: adrianofaus <adrianofaus@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 15:38:20 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/03/28 21:54:04 by coder            ###   ########.fr       */
+/*   Updated: 2022/03/28 17:14:39 by adrianofaus      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@ char	*ft_append_char(char *str, char c)
 {
 	char	*new_str;
 	int		str_size;
-
+	int		i;
+	
+	i = -1;
 	if (str)
 		str_size = ft_strlen(str);
 	else
 		str_size = 0;
 	new_str = malloc((str_size + 2) * sizeof(char));
-	new_str[str_size + 1] = '\0';
+	while (++i < str_size)
+		new_str[i] = str[i];
 	new_str[str_size] = c;
+	new_str[str_size + 1] = '\0';
 	ft_free_ptr((void *)&str);
 	return (new_str);
 }
@@ -33,12 +37,14 @@ void	expand_dollar_sign(t_list *token)
 	char	*token_content;
 	char	*expanded_content;
 	int		i;
+	char	*var;
 
 	expanded_content = NULL;
 	token_content = (char *) token->content; //token_content = helloadhjadh$USER
 	i = -1;
 	while (token_content[++i] && token_content[i] != '$')
 		expanded_content = ft_append_char(expanded_content, token_content[i]);
+	var = read_hashtable(g_tudao.hashtable[hash_string(&token_content[i + 1])], &token_content[i + 1]);
 	// while (token_content[++i] && token_content[i] != '$')
 	// {
 	// 	// se tiver aspas simples: não expande
