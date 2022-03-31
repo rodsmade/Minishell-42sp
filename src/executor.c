c3fe74af26b1fb68e80c691d2032b58e94b13078
rodsmade <rodrigogregoldo@gmail.com>
2022-04-01 00:36:07 +0200

    first attempt at new execution logic

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 22:53:25 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/03/31 20:42:30 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/04/01 00:35:28 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,14 @@ bool	is_built_in(char *str)
 			|| (ft_strncmp(str, "unset", 6) == 0)))
 		return (true);
 	return (false);
+}
+
+void	send_to_execve(t_command *command)
+{
+	char	**cmd_arr;
+
+	assemble_cmd_array();
+	execve(cmd_arr[0], cmd_arr, hashtable_to_array());
 }
 
 void	execute_built_in(t_command *command)
@@ -59,47 +67,12 @@ void	execute_command(t_command *cmd)
 {
 	if (is_built_in(cmd->cmds_with_flags->content))
 		execute_built_in(cmd);
+	else
+		send_to_execve(cmd);
 	return ;
 }
-/*
-Caso is_last_cmd seja verdadeiro eu altero o
-stdoutput para o outfile ou caso não tenha outfile eu altero para o terminal
-caso seja falso eu altero para o fd[0] do pipe criado
-*/
-// void	connect_the_dots(t_command *cmd, int is_last_cmd)
-// {
-// 	int		fd[2];
-// 	pid_t	pid;
 
-// 	if (pipe(fd) == 1)
-// 		ft_putstr_fd("Pipe", 2);
-// 	pid = fork();
-// 	if (pid == -1)
-// 		ft_putstr_fd("Fork", 2);
-// 	if (pid == 0)
-// 	{
-// 		if (cmd->outputs->content == NULL)
-// 			dup2(fd[1], STDOUT_FILENO);
-// 		else if (cmd->outputs->content != NULL)
-// 		{
-// 			open()
-// 		}
-// 		close(fd[0]);
-// 		close(fd[1]);
-// 		if (is_built_in(cmd->cmds_with_flags->content))
-// 		{
-// 			execute_built_in(cmd);
-// 			//Retornar o código de exit de acordo com a exec do built-in
-// 			exit(EXIT_SUCCESS);
-// 		}
-// 	}
-// 	dup2(fd[0], STDIN_FILENO);
-// 	close(fd[0]);
-// 	close(fd[1]);
-// 	waitpid(pid, NULL, 0);
-// }
-
-void	execute_pipelines(void)
+void	execute_main_pipeline(void)
 /**
  * TODO: Intended complete structure:
  * 		handle_redirections(pivot_cmd);
@@ -113,23 +86,29 @@ void	execute_pipelines(void)
  * 			exec_env_path();
  */
 {
-	t_list		*pivot_pipeline;
-	pid_t		pid;
+	t_list		*cmd_pivot;
+	int			pid;
 	int			wstatus;
+	t_command	*cmd;
 
-	pivot_pipeline = g_tudao.command_table.main_pipeline;
-	while (pivot_pipeline)
+	cmd_pivot = g_tudao.command_table.main_pipeline;
+	cmd = (t_command *) cmd_pivot->content;
+	if (!cmd_pivot->next && is_built_in((char *)cmd->cmds_with_flags->content))
+		execute_built_in(cmd);
+	else
 	{
-		pid = fork();
-		if (pid == -1)
-			ft_putendl_fd("error while forking", 1);
-		if (pid == 0)
+		while (cmd_pivot)
 		{
-			execute_command((t_command *) pivot_pipeline->content);
+			pid = fork();
+			if (pid == -1)
+				ft_putendl_fd("Error while forking", 1);
+			else if (pid == 0)
+				execute_command(cmd);
+			else
+				waitpid(pid, &wstatus, 0);
+			cmd_pivot = cmd_pivot->next;
+			if (cmd_pivot)
+				cmd = (t_command *) cmd_pivot->content;
 		}
-		else
-			waitpid(pid, &wstatus, 0);
-		pivot_pipeline = pivot_pipeline->next;
 	}
-	return ;
 }
