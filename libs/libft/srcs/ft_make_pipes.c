14dfc84af484fbfbbc590477e05461ab6a0e5b11
rodsmade <rodrigogregoldo@gmail.com>
2022-04-22 00:39:17 +0200

    fixed poor exception handling in ft_make_pipes

--- libs/libft/srcs/ft_make_pipes.c
+++ libs/libft/srcs/ft_make_pipes.c
@@ -6,36 +6,38 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:38:02 by adrianofaus       #+#    #+#             */
-/*   Updated: 2022/04/07 23:56:04 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/04/22 00:33:09 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	free_previous_pipes(int ***pipes_arr, int i)
+{
+	while (--i >= 0)
+		ft_free_ptr((void *)pipes_arr[i]);
+	return ;
+}
+
 int	**ft_make_pipes(int total_pipes)
 {
 	int	i;
-	int	**pipes;
+	int	**pipes_arr;
 
 	if (total_pipes == 0)
 		return (NULL);
-	pipes = (int **)malloc(total_pipes * sizeof(int *));
-	if (!pipes)
+	pipes_arr = (int **)malloc(total_pipes * sizeof(int *));
+	if (!pipes_arr)
+		return (NULL);
+	i = -1;
+	while (++i < total_pipes)
 	{
-		ft_putendl_fd("Malloc error", 2);
-		exit(EXIT_FAILURE);
-	}
-	i = 0;
-	while (i < total_pipes)
-	{
-		pipes[i] = (int *)malloc(2 * sizeof(int));
-		if (!pipes[i])
+		pipes_arr[i] = (int *)malloc(2 * sizeof(int));
+		if (!pipes_arr[i] || !pipe(pipes_arr[i]))
 		{
-			ft_putendl_fd("Malloc error", 2);
-			exit(EXIT_FAILURE);
+			free_previous_pipes(&pipes_arr, i);
+			return (NULL);
 		}
-		pipe(pipes[i]);
-		i++;
 	}
-	return (pipes);
+	return (pipes_arr);
 }
